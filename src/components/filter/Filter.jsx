aab4937944c715a7b8aee51ef1a2fdabd4a754da
Maistrenko Roman <maistrenko.roman@gmail.com>
2025-02-21 17:22:04 +0200

    21.02.2025

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./Filter.css"
 
-const Filter = (label) => {
+const Filter = ({label, name, checked, onChange}) => {
     return (
         <div className="filter__container">
             <label className="container">
-                <input type="checkbox" defaultChecked="checked"/>
+                <input type="checkbox" checked={checked || false} onChange={onChange} name={name}/>
                 <div className="line"/>
                 <div className="line line-indicator"/>
             </label>
-            <span className="label_text">{label.label}</span>
+            <span className="label_text">{label}</span>
         </div>
     );
 };
--- src/components/filterList/FilterList.jsx
+++ src/components/filterList/FilterList.jsx
@@ -4,11 +4,20 @@ import Filter from "../filter/Filter.jsx";
 
 const FilterList = ({ filterItems }) => {
 
+    const [selectedFilters, setSelectedFilters] = React.useState({});
+
+    const handleFilterChange = (name, value) => {
+        setSelectedFilters((prevState) => ({
+            ...prevState,
+            [name]: prevState[name] === value ? null : value
+        }))
+    }
+
     const renderFilterGroup = (section) => (
         <div className={styles.filter_group} key={section.title}>
             <h1 className={styles.filter__title}>{section.title}</h1> {/* Заголовок для групи */}
             {section.items.map((item, index) => (
-                <Filter key={index} label={item} />
+                <Filter key={index} label={item} name={section.title} checked={selectedFilters[section.title] === item} onChange={() => handleFilterChange(section.title, item)} />
             ))}
         </div>
     );
